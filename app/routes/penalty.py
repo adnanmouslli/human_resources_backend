92dfdc85b6d60ffcec0c4564fa1ebba282c46f29
omar matoki <omarmatoki61@gmail.com>
2025-04-21 13:16:50 +0300

    reward and penalty controllers,routes

--- app/routes/penalty.py
+++ app/routes/penalty.py
@@ -1,50 +1,43 @@
-# app/routes/penalty_routes.py
-
-from flask import Blueprint
-from app.controllers.penalty_controller import (
-    create_penalty,
-    get_all_penalties,
-    get_penalty_by_id,
-    update_penalty,
-    delete_penalty,
-    get_penalties_by_employee_id
-)
+from flask import Blueprint, request, jsonify
+from app.controllers.penalty_controller import PenaltyController
 from app.utils import token_required
 
 penalties_bp = Blueprint('penalties', __name__)
 
-# Create Penalty
 @penalties_bp.route('/api/penalties', methods=['POST'])
 @token_required
-def create():
-    return create_penalty()
+def create_penalty(user_id):
+    data = request.get_json()
+    response, status_code = PenaltyController.create_penalty(data)
+    return jsonify(response), status_code
 
-# Get All Penalties with Employee Details
 @penalties_bp.route('/api/penalties', methods=['GET'])
 @token_required
-def get_all():
-    return get_all_penalties()
+def get_all_penalties(user_id):
+    response, status_code = PenaltyController.get_all_penalties()
+    return jsonify(response), status_code
 
-# Get Penalty by ID
 @penalties_bp.route('/api/penalties/<int:id>', methods=['GET'])
 @token_required
-def get_by_id(id):
-    return get_penalty_by_id(id)
+def get_penalty(user_id, id):
+    response, status_code = PenaltyController.get_penalty_by_id(id)
+    return jsonify(response), status_code
 
-# Update Penalty
 @penalties_bp.route('/api/penalties/<int:id>', methods=['PUT'])
 @token_required
-def update(id):
-    return update_penalty(id)
+def update_penalty(user_id, id):
+    data = request.get_json()
+    response, status_code = PenaltyController.update_penalty(id, data)
+    return jsonify(response), status_code
 
-# Delete Penalty
 @penalties_bp.route('/api/penalties/<int:id>', methods=['DELETE'])
 @token_required
-def delete(id):
-    return delete_penalty(id)
+def delete_penalty(user_id, id):
+    response, status_code = PenaltyController.delete_penalty(id)
+    return jsonify(response), status_code
 
-# Get Penalties by Employee ID
 @penalties_bp.route('/api/penalties/employee/<int:emp_id>', methods=['GET'])
 @token_required
-def get_by_employee_id(emp_id):
-    return get_penalties_by_employee_id(emp_id)
+def get_penalties_by_employee(user_id, emp_id):
+    response, status_code = PenaltyController.get_penalties_by_employee_id(emp_id)
+    return jsonify(response), status_code
--- app/routes/reward.py
+++ app/routes/reward.py
@@ -1,50 +1,43 @@
-# app/routes/reward_routes.py
-
-from flask import Blueprint
-from app.controllers.reward_controller import (
-    create_reward,
-    get_all_rewards,
-    get_reward_by_id,
-    update_reward,
-    delete_reward,
-    get_rewards_by_employee_id
-)
+from flask import Blueprint, request, jsonify
+from app.controllers.reward_controller import RewardController
 from app.utils import token_required
 
 rewards_bp = Blueprint('rewards', __name__)
 
-# Create Reward
 @rewards_bp.route('/api/rewards', methods=['POST'])
 @token_required
-def create():
-    return create_reward()
+def create_reward(user_id):
+    data = request.get_json()
+    response, status_code = RewardController.create_reward(data)
+    return jsonify(response), status_code
 
-# Get All Rewards with Employee Details
 @rewards_bp.route('/api/rewards', methods=['GET'])
 @token_required
-def get_all():
-    return get_all_rewards()
+def get_all_rewards(user_id):
+    response, status_code = RewardController.get_all_rewards()
+    return jsonify(response), status_code
 
-# Get Reward by ID
 @rewards_bp.route('/api/rewards/<int:id>', methods=['GET'])
 @token_required
-def get_by_id(id):
-    return get_reward_by_id(id)
+def get_reward(user_id, id):
+    response, status_code = RewardController.get_reward_by_id(id)
+    return jsonify(response), status_code
 
-# Update Reward
 @rewards_bp.route('/api/rewards/<int:id>', methods=['PUT'])
 @token_required
-def update(id):
-    return update_reward(id)
+def update_reward(user_id, id):
+    data = request.get_json()
+    response, status_code = RewardController.update_reward(id, data)
+    return jsonify(response), status_code
 
-# Delete Reward
 @rewards_bp.route('/api/rewards/<int:id>', methods=['DELETE'])
 @token_required
-def delete(id):
-    return delete_reward(id)
+def delete_reward(user_id, id):
+    response, status_code = RewardController.delete_reward(id)
+    return jsonify(response), status_code
 
-# Get Rewards by Employee ID
 @rewards_bp.route('/api/rewards/employee/<int:emp_id>', methods=['GET'])
 @token_required
-def get_by_employee_id(emp_id):
-    return get_rewards_by_employee_id(emp_id)
+def get_rewards_by_employee(user_id, emp_id):
+    response, status_code = RewardController.get_rewards_by_employee_id(emp_id)
+    return jsonify(response), status_code
