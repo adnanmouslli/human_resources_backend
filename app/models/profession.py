79a596325d626d0c3aca8744d9fa5e3c0bbc3b9c
Adnan Mouslli <yazanmouslli6@gmail.com>
2025-05-15 16:13:30 -0700

    update profession

--- app/models/profession.py
+++ app/models/profession.py
@@ -6,6 +6,11 @@ class Profession(db.Model):
     __tablename__ = 'professions'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # ID تلقائي
-    name = db.Column(db.String(100), nullable=False)  #
+    name = db.Column(db.String(100), nullable=False)  # اسم المهنة
+    hourly_rate = db.Column(db.Numeric(10, 2), nullable=False)  # سعر الساعة
+    daily_rate = db.Column(db.Numeric(10, 2), nullable=False)  # سعر اليوم
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())  # تاريخ الإضافة
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())  # تاريخ التحديث
+
 def __repr__(self):
     return f"<Profession {self.name}>"
